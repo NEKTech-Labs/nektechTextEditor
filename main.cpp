e0065f245940bbe31005a891d0bbf07fbecbaed8
NEK Tech Labs <pankaj.saraf@nektech.in>
2015-03-23 21:06:48 +0530

    Update main.cpp
    Adding Header.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,25 @@
+/*
+ * main.cpp - nektechTextEditor
+ *
+ * Copyright (C) NEK Tech 2013
+ * Developers V1.0:  	Pankaj Saraf
+ *			            Jitendra Khasdev
+ *                      Pooja Shukla
+ *                      Akshay Rastogi
+ *                      Poovika Ujjaini
+ * Copyright (C) NEK Tech 2015
+ *
+ * Author and Architect: Pankaj Saraf
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms and conditions of NEK Tech.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ */
+
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include "nektech_doc_handler.h"
